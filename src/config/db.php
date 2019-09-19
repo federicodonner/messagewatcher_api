a34805c5776dfe73f86f3785f844a5bf4a58a491
Federico Donner <federico.donner@gmail.com>
2019-09-19 20:02:19 -0300

    data(db): add production credentials

--- src/config/db.php
+++ src/config/db.php
@@ -6,17 +6,21 @@ class db{
 /*
   // Properties
   private $dbhost = 'localhost';
-  private $dbuser = 'feder161_cladmin';
-  private $dbpass = 'cladmin';
-  private $dbname = 'feder161_clublibros';
+  private $dbuser = 'feder161_mgadmin';
+  private $dbpass = 'GgCy2p2VLGnBAC3';
+  private $dbname = 'feder161_messagewatcher';
 
-*/
+// */
+
+
 
 //MAMP
    private $dbhost = 'localhost';
    private $dbuser = 'root';
    private $dbpass = 'root';
    private $dbname = 'messagewatcher';
+
+// */
 
   public function connect(){
     $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname;charset=UTF8";
