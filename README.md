56e7ba01686975c365884d363693109e26d5d2b2
Federico Donner <federico.donner@gmail.com>
2019-09-18 22:22:53 -0300

    build (app): initial build Copy of clublibros API. No changes.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # messagewatcher_api
+
+Two endpoints are exposed in the API:
+- [URL] GET. Returns JSON with the list of the configured campaigns.
+- [URL] POST. Reports campaign error when the message was not received.
---
+++ src/config/db.php
@@ -0,0 +1,24 @@
+<?php
+
+class db{
+
+  // Properties
+  private $dbhost = 'localhost';
+  private $dbuser = 'feder161_cladmin';
+  private $dbpass = 'cladmin';
+  private $dbname = 'feder161_clublibros';
+
+//MAMP
+  // private $dbhost = 'localhost';
+  // private $dbuser = 'root';
+  // private $dbpass = 'root';
+  // private $dbname = 'feder161_clublibros';
+
+  public function connect(){
+    $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname;charset=UTF8";
+    $dbConnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass);
+    $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $dbConnection;
+  }
+}
