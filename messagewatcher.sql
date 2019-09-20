b6e012c457047351d5320d5971cd1be3f9ecfba6
federicodonner <federico.donner@gmail.com>
2019-09-20 13:34:47 -0300

    build(sql): update database

--- messagewatcher.sql
+++ messagewatcher.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Sep 19, 2019 at 11:30 AM
+-- Generation Time: Sep 20, 2019 at 04:33 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -42,6 +42,29 @@ CREATE TABLE `campaigns` (
   `active` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keepalive`
+--
+
+CREATE TABLE `keepalive` (
+  `id` int(11) NOT NULL,
+  `keepalive_date` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `id` int(11) NOT NULL,
+  `message_date` int(15) NOT NULL,
+  `message_text` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Indexes for dumped tables
 --
@@ -53,6 +76,18 @@ ALTER TABLE `campaigns`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `keepalive`
+--
+ALTER TABLE `keepalive`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,6 +95,18 @@ ALTER TABLE `campaigns`
 -- AUTO_INCREMENT for table `campaigns`
 --
 ALTER TABLE `campaigns`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `keepalive`
+--
+ALTER TABLE `keepalive`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
