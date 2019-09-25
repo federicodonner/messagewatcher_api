95e06adca3242541226b05808b8de900efc13703
federicodonner <federico.donner@gmail.com>
2019-09-25 08:53:23 -0300

    feat(report): add GET interface to endpoint
    Data is received via queryparams text and shorcode

--- src/routes/report.php
+++ src/routes/report.php
@@ -35,3 +35,37 @@ $app->post('/api/report', function (Request $request, Response $response) {
         echo '{"error":{"text": '.$e->getMessage().'}}';
     }
 });
+
+
+// Add product
+$app->get('/api/report', function (Request $request, Response $response) {
+
+    $text = $request->getQueryParam('text');
+    $shortcode = $request->getQueryParam('shortcode');
+    $timestamp = time()-10800;
+
+      $sql = "INSERT INTO reports (message_date,message_text,shortcode) VALUES (:message_date,:message_text,:shortcode)";
+
+    try {
+        // Get db object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':message_date', $timestamp);
+        $stmt->bindParam(':message_text', $text);
+        $stmt->bindParam(':shortcode', $shortcode);
+
+        $stmt->execute();
+
+        $newResponse = $response->withStatus(200);
+        $body = $response->getBody();
+        $body->write('{"status": "success","message": "report added"}');
+        $newResponse = $newResponse->withBody($body);
+        return $newResponse;
+    } catch (PDOException $e) {
+        echo '{"error":{"text": '.$e->getMessage().'}}';
+    }
+});
